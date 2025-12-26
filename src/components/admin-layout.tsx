fecec1419eccdd2e3ed86560599a20d80e06d9bc
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-27 00:46:19 +0530

    fix: adjust layout styles for improved visual consistency

--- src/components/admin-layout.tsx
+++ src/components/admin-layout.tsx
@@ -4,11 +4,11 @@ import { Header } from "@/components/header";
 
 export function AdminLayout() {
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="flex min-h-screen ">
       <Sidebar />
       <div className="flex-1 flex flex-col ml-70">
         <Header />
-        <main className="flex-1 bg-[#E8EFF9]">
+        <main className="flex-1 bg-[#E8EFF9] pb-5 ">
           <Outlet />
         </main>
       </div>
