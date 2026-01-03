da06f3d40c1f26b9af20c765d12e083f2b244d69
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-01-03 18:23:40 +0530

    feat: implement sidebar toggle functionality in admin layout

--- src/components/admin-layout.tsx
+++ src/components/admin-layout.tsx
@@ -1,14 +1,25 @@
 import { Outlet } from "react-router";
 import { Sidebar } from "@/components/sidebar";
 import { Header } from "@/components/header";
+import { useState } from "react";
 
 export function AdminLayout() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
-    <div className="flex min-h-screen ">
-      <Sidebar />
-      <div className="flex-1 flex flex-col ml-70">
-        <Header />
-        <main className="flex-1 bg-[#E8EFF9] pb-5 ">
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+      <div className="flex-1 flex flex-col ml-0 lg:ml-70 overflow-auto">
+        <Header onMenuClick={toggleSidebar} />
+        <main className="flex-1 bg-[#E8EFF9] pb-5">
           <Outlet />
         </main>
       </div>
